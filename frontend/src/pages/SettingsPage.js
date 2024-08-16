707d3f33823ad48b2c07042e2f1845aef6c4128b
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-16 17:21:25 +0400

    Create SettingsPage.js

---
+++ frontend/src/pages/SettingsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SettingsPage() {
+  return (
+    <div>
+      <h2>Settings</h2>
+      {/* Add settings options and functionalities */}
+    </div>
+  );
+}
+
+export default SettingsPage;
