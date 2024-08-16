402ad15575f56d4954d225ce684115b162b6c204
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-16 17:21:11 +0400

    Create ProfilePage.js

---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProfilePage() {
+  return (
+    <div>
+      <h2>Your Profile</h2>
+      {/* Add profile details and functionalities */}
+    </div>
+  );
+}
+
+export default ProfilePage;
