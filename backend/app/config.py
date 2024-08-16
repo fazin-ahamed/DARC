e1822f131ef2083438b115e17726b6e5a8506f4c
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-16 14:02:31 +0400

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://username:password@localhost:5432/darc_db"
+    database_url: str = "postgresql://postgres.ccpighleiemxslknuvyh:fazin1980dxb@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
     secret_key: str = "your_secret_key_here"
     jwt_secret_key: str = "your_jwt_secret_key_here"
     websocket_port: int = 8001
