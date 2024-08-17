3e465d2cea51776291520797449d4949e6751bf2
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-17 07:43:09 +0400

    Create dashboard.js

---
+++ frontend/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Welcome to the DARC Dashboard</h1>
+            {/* Add dashboard features here */}
+        </div>
+    );
+}
+
+export default Dashboard;
