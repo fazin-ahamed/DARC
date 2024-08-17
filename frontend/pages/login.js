c978ccf9eb3f46f45b573ffa9053576d55d05692
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-17 07:40:59 +0400

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -9,7 +9,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}/login`, {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
         username,
         password,
       })
