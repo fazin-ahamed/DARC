afd76aa07a1f828566a1de43e00ecbff952bf006
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-22 20:22:49 +0400

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,21 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/globals.css';
 
-export default function Home() {
+function Home() {
   return (
     <div>
-      <head>
-        <title>Home Page</title>
-        <meta name="description" content="Home Page of the DARC Project" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-
-      <main>
-        <h1>Welcome to the School Project By Fazin</h1>
+      <nav>
+        <Link to="/" className="link-spacing">Home</Link>
         <Link to="/login" className="link-spacing">Login</Link>
         <Link to="/signup" className="link-spacing">Sign Up</Link>
-        <Link to="/dashboard" className="link-spacing"><Dashboard</Link>
+        <Link to="/dashboard" className="link-spacing">Dashboard</Link>
+      </nav>
+      <main>
+        <h1>Welcome to the Home Page</h1>
       </main>
     </div>
   );
 }
+
+export default Home;
