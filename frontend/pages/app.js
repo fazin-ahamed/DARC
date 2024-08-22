f5882afb2552aa842f41f8114da6cd8e9064a729
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-08-22 10:14:58 +0400

    Update app.js

--- frontend/pages/app.js
+++ frontend/pages/app.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ErrorBoundary from './errorboundary'; // Adjust the path as needed
+import Dashboard from './dashboard'; // Adjust the path as needed
+
+const App = () => (
+    <ErrorBoundary>
+        <Dashboard />
+    </ErrorBoundary>
+);
+
+export default App;
 
