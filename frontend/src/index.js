49308222276378827e70fdde28f91d12a25b7a9a
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-09-02 22:15:31 +0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Login from './login';
-import Signup from './signup';
-import Dashboard from './dashboard'; // Import the Dashboard component
+import ReactDOM from 'react-dom';
+import AppRouter from './App'; // Import the Router component
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* Add the Dashboard route */}
-      </Routes>
-    </Router>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+    <React.StrictMode>
+        <AppRouter />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
