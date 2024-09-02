99256c08695bb9967df7f132c3e5d4ccc7cb6869
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-09-03 00:51:41 +0400

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './app'; // Rename if necessary
 import Dashboard from './dashboard'; // Import your Dashboard component
+import './monaco-loader';
 
 const AppRouter = () => (
     <Router>
