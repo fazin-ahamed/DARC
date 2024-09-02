23f6b92ae3ffefacef058c4793d537d4e8e3d717
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2024-09-02 22:16:25 +0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './App'; // Import the Router component
+import AppRouter from './app'; // Import the Router component
 
 ReactDOM.render(
     <React.StrictMode>
